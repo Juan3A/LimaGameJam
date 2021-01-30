f7ad89acf5167c9b8a5f6c8f1ac96375e4d26407
Luis Gonzales <GonzalesRengifoLE@outlook.com>
2021-01-29 22:09:39 -0500

    Menu,transicion escenas

---
+++ LimaGameJam2021/Assets/Scripts/LoadingScreen/LoadingBar.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LoadingBar : MonoBehaviour
+{
+    private Image slide;
+
+    public GameObject text;
+    public Text loadingT;
+
+    void Start()
+    {
+        slide = gameObject.GetComponent<Image>();
+        loadingT = loadingT.gameObject.GetComponent<Text>();
+        text.SetActive(false);
+        StartCoroutine(LoadAsync());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    IEnumerator LoadAsync()
+    {
+        AsyncOperation ao = SceneManager.LoadSceneAsync(SceneLoad.SceneTO);
+        ao.allowSceneActivation = false;
+
+
+        while (!ao.isDone)
+        {
+
+            slide.fillAmount = ao.progress+.1f;
+            loadingT.text = (ao.progress+.1f) * 100 + " %";
+
+            if (ao.progress >= .9f)
+            {
+
+                text.SetActive(true);
+
+                if (Input.GetKeyDown(KeyCode.Space))
+                {
+
+                    ao.allowSceneActivation = true;
+                    SceneLoad.Reset();
+                }
+
+                yield return null;
+            }
+
+        }
+    }
+}
---
+++ LimaGameJam2021/Assets/Scripts/Manu/Manu.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Manu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void GotoScene(string scene)
+    {
+        SceneLoad.TranscionesScene(scene);
+    }
+
+    public void ExitGame()
+    {
+
+        Application.Quit();
+
+    }
+}
+public static class SceneLoad
+{
+
+    public static string SceneTO;
+    public static string Interlude = "LoadingScreen";
+
+    public static void TranscionesScene(string sceneT)
+    {
+        SceneTO = sceneT;
+        SceneManager.LoadScene(SceneLoad.Interlude);
+    }
+    public static void Reset()
+    {
+        SceneTO = "Menu";
+    }
+
+}
+
